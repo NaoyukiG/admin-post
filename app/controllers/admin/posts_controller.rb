d3b08891e728a75c2d81c2ff99012bc88a653cc1
NaoyukiG <naoyukineo@gmail.com>
2022-07-29 00:00:33 +0900

    postモデルの作成

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,2 +1,26 @@
 class Admin::PostsController < ApplicationController
+  before_action :if_not_admin
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def if_not_admin
+    redirect_to root_path unless current_user.admin?
+  end
+
+  def post_params
+    params.rewuire(:post).permit(:text).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
   resources :posts, only: [:index]
+
+  namespace :admin do
+    resources :post, only: [:new, :create, :edit, :destroy]
+  end
 end
