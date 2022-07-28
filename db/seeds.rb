2afd3f83965a83d1b6a589a0c32ddf018cead362
NaoyukiG <naoyukineo@gmail.com>
2022-07-28 23:06:49 +0900

    seed.rbへadminの記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(nickname: "管理者",
+  email: "admin@email",
+  password: "aaaaaa",
+  password_confirmation: "aaaaaa",
+  admin: true)
